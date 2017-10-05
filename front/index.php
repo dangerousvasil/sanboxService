23ce34bedbf28294032adc0d3e3d0610ba49c3d5
pvv <79067000905@ya.ru>
2017-10-05 10:43:32 +0300

    add files

--- front/index.php
+++ front/index.php
@@ -4,16 +4,17 @@ require_once 'vendor/autoload.php';
 
 use JsonRPC\Client;
 
-$client = new Client('http://web/index.php');
-
+// here set up balanced nginx
+$client = new Client('http://web/');
 $http = $client->getHttpClient()
        ->withDebug()
+    // service set up here
        ->withHeaders(['Host: service']);
 
 try {
     $result = $client->execute('addition', [3, 5]);
 } catch (Exception $e) {
-
+    // debug info is disgusting
     var_dump($e);
 }
 var_dump($result);
